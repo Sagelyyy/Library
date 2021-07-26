05020358027ed7ff10435567e7910aa70bdd3dcd
Chris <orj.tsuk@gmail.com>
2021-07-25 22:57:37 -0400

    Added a dynamic dropdown menu to the html for now, that updates with all the objects in the myLibrary array whenever you add a new book. Still need to display the books info on the page.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const dropdown = document.querySelector('select')
+
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -5,7 +9,34 @@ function Book(title, author, pages, read){
     this.author = author
     this.pages = pages
     this.read = read
-    this.info = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
+
+Book.prototype.info = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
+
+function addBookToLibrary(title, author, pages, read){
+    let book = new Book(title, author, pages, read)
+    myLibrary.push(book)
+    addBooksToList()
+}
+
+function addBooksToList(){
+    clearList()
+    for (i = 0; i < myLibrary.length; i++){
+        let newOption = document.createElement('option');
+        newOption.className = 'book-entry'
+        newOption.value = myLibrary[i].title
+        newOption.innerText = myLibrary[i].title
+        dropdown.appendChild(newOption)
+    }
+}
+
+function clearList(){
+    ddList = document.getElementsByClassName('book-entry')
+    if(ddList.length > 0){
+        for (i = 0; i < ddList.length; i++){
+            dropdown.removeChild(ddList[i])
+        }
     }
 }
