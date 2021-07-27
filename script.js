9edafdede77e91f9833c15f7076bb1b5a6ddaf87
Chris <orj.tsuk@gmail.com>
2021-07-26 21:44:11 -0400

    fixed bug that didn't remove all the books from the list when calling clearList

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const dropdown = document.querySelector('select')
+const container = document.querySelector('.book-container')
 
+const dropdown = document.querySelector('select')
 const cardTitle = document.querySelector('.title')
 const cardAuthor = document.querySelector('.author')
 const cardPages = document.querySelector('.pages')
 const cardRead = document.querySelector('.read')
+const newBook = document.querySelector('.new-book')
+newBook.addEventListener('click', createForm())
 
 selectBook()
 let myLibrary = [];
@@ -17,6 +20,21 @@ function Book(title, author, pages, read){
 
 Book.prototype.info = function(){
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
+}
+
+function createForm(){
+        const titleForm = document.createElement('form')
+        titleForm.className = 'title-form'
+        container.appendChild(titleForm)
+        const authorForm = document.createElement('form')
+        authorForm.className = 'author-form'
+        container.appendChild(authorForm)
+        const pagesForm = document.createElement('form')
+        pagesForm.className = 'pages-form'
+        container.appendChild(pagesForm)
+        const readForm = document.createElement('form')
+        readForm.className = 'read-form'
+        container.appendChild(readForm)
 }
 
 function addBookToLibrary(title, author, pages, read){
@@ -38,7 +56,7 @@ function addBooksToList(){
 
 
 function selectBook(){
-    eListener = dropdown.addEventListener('change', function(e){
+    const eListener = dropdown.addEventListener('change', function(e){
         for(i = 0; i < myLibrary.length; i++){
             if(myLibrary[i].title.toLowerCase() == e.target.value){
                 cardTitle.textContent = e.target.value
@@ -51,13 +69,8 @@ function selectBook(){
 }
 
 function clearList(){
-    ddList = document.getElementsByClassName('book-entry')
-    if(ddList.length > 0){
-        for (i = 0; i < ddList.length; i++){
-            dropdown.removeChild(ddList[i])
-        }
+    let ddList = document.getElementsByClassName('book-entry')
+    while (ddList.length > 0){
+        ddList[0].parentNode.removeChild(ddList[0])
     }
 }
-
-addBookToLibrary('The Best puppy', 'Amanda Weiskopf', 50000, 'unread')
-addBookToLibrary('The Best kitty', 'Chris Weiskopf', 100, 'read')
